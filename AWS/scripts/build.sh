410812d340383013fb47dc23acd00f30982da0e9
armendkadriaj <kadriajarmend@gmail.com>
2022-12-15 14:23:01 +0100

    Update build.sh

--- AWS/scripts/build.sh
+++ AWS/scripts/build.sh
@@ -1,1 +1,1 @@
-docker build -t flight-search-app --build-arg EC2_INSTANCE_IP='3.87.174.127' /home/ec2-user/FlightSearchApp
+docker build -t flight-search-app --build-arg EC2_INSTANCE_IP='54.86.91.138' /home/ec2-user/FlightSearchApp
